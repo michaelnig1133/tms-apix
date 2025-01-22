4af8e6b66a57c3ebb6bbb3bbafa59fc7fa4c35af
unknown <mebrhit765@gmail.com>
2025-01-22 10:58:25 +0300

    requirement.txt added

--- tms_backend/settings.py
+++ tms_backend/settings.py
@@ -19,7 +19,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True 
-
+EMAIL_HOST_USER="mebrhit765@gmail.com"
+EMAIL_HOST_PASSWORD="pdaa ybew lspa osxb"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
